b5f99f5298190f78aed8f7a2b105a94c23dc42e3
sjf0115 <1203745031@qq.com>
2023-04-24 23:26:56 +0800

    MOD: SpringBootHello

---
+++ spring-boot-hello/src/main/java/com/spring/example/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能：HelloApplication 引导类
+ * 作者：SmartSi
+ * CSDN博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2023/4/24 下午11:24
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
