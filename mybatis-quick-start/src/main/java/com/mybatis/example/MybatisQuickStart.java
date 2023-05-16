7b436cc02e7eede101b55623cf615a67f2827988
sjf0115 <1203745031@qq.com>
2023-05-16 23:53:30 +0800

    MOD: Mybatis 快速入门

---
+++ mybatis-quick-start/src/main/java/com/mybatis/example/MybatisQuickStart.java
@@ -0,0 +1,40 @@
+package com.mybatis.example;
+
+import com.mybatis.example.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * 功能：入口
+ * 作者：SmartSi
+ * CSDN博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2023/5/16 下午11:40
+ */
+public class MybatisQuickStart {
+    public static void main(String[] args) throws IOException {
+        // 加载 Mybatis 的核心配置文件 mybatis-config.xml
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession session = sqlSessionFactory.openSession();
+
+        // 查询所有的用户
+            // 参数是 SQL 语句的唯一标识 Mapper.xml 文件中定义
+        List<User> users = session.selectList("com.mybatis.example.UserMapper.selectAll");
+        for (User user : users) {
+            System.out.println("全部用户: " + user);
+        }
+
+        // 根据指定的ID查询用户
+        User user = session.selectOne("com.mybatis.example.UserMapper.selectById", 1);
+        System.out.println("目标用户: " + user);
+    }
+}
---
+++ mybatis-quick-start/src/main/java/com/mybatis/example/pojo/User.java
@@ -0,0 +1,61 @@
+package com.mybatis.example.pojo;
+
+/**
+ * 功能：User实体类
+ * 作者：SmartSi
+ * CSDN博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2023/5/16 下午11:22
+ */
+public class User {
+    // 主键ID
+    private long id;
+    // '姓名'
+    private String name;
+    // 年龄
+    private int age;
+    // '邮箱'
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
